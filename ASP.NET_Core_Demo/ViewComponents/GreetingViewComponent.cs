e7e1a200c6c487529228088ac9bc0d0fb8d1ea0a
Patrick Todd <patrick.todd@reisystems.com>
2017-07-15 19:56:05 -0400

    added viewcomponents and partial view

---
+++ ASP.NET_Core_Demo/ViewComponents/GreetingViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using AspNetCoreDemo.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreDemo.ViewComponents
+{
+    public class GreetingViewComponent : ViewComponent
+    {
+        private IGreeter _greeter;
+
+        public GreetingViewComponent(IGreeter greeter)
+        {
+            _greeter = greeter;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var model = _greeter.GetGreeting();
+            return View("Default", model);
+        }
+    }
+}
